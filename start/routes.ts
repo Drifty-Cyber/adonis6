92b8d65db91902bff362adcbcf5094be09e336aa
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-06-11 11:21:11 +0100

    chore: add routes

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,14 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+router.get('/welcome', async (ctx) => {
+  return ctx.view.render('pages/welcome')
+})
+
+router
+  .group(() => {
+    router.get('/home', async () => {
+      return 'Home Route'
+    })
+  })
+  .as('Home')
